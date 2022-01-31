d4fead9f52d8a0342ded56e1179e38020fee376b
Adrian Taylor <adetaylor@chromium.org>
2022-01-31 14:57:20 -0800

    Pass by copy - even more doomed

--- src/value_param.rs
+++ src/value_param.rs
@@ -1,3 +1,4 @@
+use cxx::{UniquePtr, memory::UniquePtrTarget};
 // Copyright 2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,34 +12,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use moveit::{CopyNew, New};
+use moveit::{CopyNew, New, MoveNew, DerefMove};
 
-use std::{mem::MaybeUninit, pin::Pin};
+use std::{mem::MaybeUninit, pin::Pin, ops::Deref};
 
 /// A trait which is used to receive any C++ parameter passed by value.
-/// This trait is implemented both for references `&T` and for `T` itself,
+/// This trait is implemented both for references `&T` and for `UniquePtr<T>`,
 /// subject to the presence or absence of suitable copy and move constructors.
 /// This allows you to pass in parameters by copy (as is ergonomic and normal
 /// in C++) retaining the original parameter; or by move semantics thus
 /// destroying the object you're passing in. Simply use a reference if you want
 /// copy semantics, or the item itself if you want move semantics.
-/// It is not recommended that you implement this trait. If you want to do
-/// something out of the ordinary here, instead implement [`New`] - there's
-/// a blanket implementation which will implement this trait for anything
-/// that implements [`New`].
+/// It is not recommended that you implement this trait.
 pub trait ValueParam<T> {
     unsafe fn new(self, this: Pin<&mut MaybeUninit<T>>);
 }
-
-// impl<T> ValueParam<T> for T
-// where
-//     T: New<Output = T>,
-// {
-//     type Output = T;
-//     unsafe fn new(self, this: Pin<&mut MaybeUninit<Self::Output>>) {
-//         self.new(this);
-//     }
-// }
 
 impl<T> ValueParam<T> for &T
 where
@@ -49,15 +37,12 @@ where
     }
 }
 
-// impl<'a, T> ValueParam<T> for T
-// where
-//     T: 'a + MoveNew,
-//     &'a mut T: DerefMove,
-//     &'a mut T: Deref<Target = T>
-// {
-//     unsafe fn new(mut self, this: Pin<&mut MaybeUninit<T>>) {
-//         let pin = std::pin::Pin::new_unchecked(&mut self);
-//         let cons = crate::moveit::new::mov(pin);
-//         cons.new(this);
-//     }
-// }
+impl<'a, T> ValueParam<T> for UniquePtr<T>
+where
+    T: 'a + MoveNew + UniquePtrTarget,
+    &'a mut T: DerefMove + Deref<Target=T>
+{
+    unsafe fn new(mut self, this: Pin<&mut MaybeUninit<T>>) {
+        crate::moveit::new::mov(self.pin_mut()).new(this)
+    }
+}
